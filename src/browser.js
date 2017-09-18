876cad0b97f67c437ce305b0b1a3a79fad7baa1d
Edgar <komono.32@gmail.com>
2017-09-18 08:59:10 +0200

    addconst cases

--- src/browser.js
+++ src/browser.js
@@ -51,6 +51,24 @@ let tests = [{
       this.value++;
     }
   }
+}, {
+  name: 'const vs var',
+  fName: 'var',
+  sName: 'const',
+
+  value: 0,
+  first: function () {
+    var x = 5;
+    if (x) {
+      this.value++;
+    }
+  },
+  second: function () {
+    const x = 5;
+    if (x) {
+      this.value++;
+    }
+  }
 }];
 
 /**
