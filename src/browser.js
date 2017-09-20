32ecbe64266c8a8138f932a946942c3171d877cf
Edgar <komono.32@gmail.com>
2017-09-20 09:08:41 +0200

    experimenting new method for measuring performance

--- src/browser.js
+++ src/browser.js
@@ -1,3 +1,76 @@
+
+
+(function () {
+  const test = {
+    first: function () {
+      var u = {a: 1, b: 2},
+        a = u.a,
+        b = u.b;
+    },
+    second: function () {
+      var u = {a: 1, b: 2},
+        {a, b} = u;
+    }
+  };
+
+  var fTestDouble = function (f1, f2, nInnerRepetitions, nRepetitions) {
+    let i, j,
+      d1 = 0,
+      d2 = 0;
+
+    for (i = 0; i < nRepetitions; i++) {
+
+      for (j = 0; j < nInnerRepetitions; j++) {
+        d1 += fTestSingle('test1', f1, nInnerRepetitions);
+      }
+      for (j = 0; j < nInnerRepetitions; j++) {
+        d2 += fTestSingle('test2', f2, nInnerRepetitions);
+      }
+    }
+
+    const avDuration1 = d1 / (nInnerRepetitions * nRepetitions),
+      avDuration2 = d2 / (nInnerRepetitions * nRepetitions);
+
+    return {
+      avDuration1: avDuration1 * 1000,
+      avDuration2: avDuration2 * 1000
+    };
+  };
+
+  var fTestSingle = function (testName, f1, nRepetitions) {
+    performance.mark(`${testName}-start`);
+  
+    let i = 0;
+    for (; i < nRepetitions; i++) {
+      f1();
+    }
+
+    performance.mark(`${testName}-end`);
+    performance.measure(testName, `${testName}-start`, `${testName}-end`);
+    let measures = performance.getEntriesByName(testName);
+
+    performance.clearMarks();
+    performance.clearMeasures();
+
+    return measures[0].duration;
+  };
+
+
+  const N_LOOP_REPETITIONS = 800;
+  const N_INNER_REPETITIONS = 200;
+
+
+
+  const res = fTestDouble(test.first, test.second, N_INNER_REPETITIONS, N_LOOP_REPETITIONS);
+
+  console.log("Double test:", res.avDuration1, res.avDuration2);
+
+})();
+
+
+if(false) {
+
+
 let tests = [{
   name: 'const vs var',
   fName: 'var',
@@ -54,7 +127,6 @@ let tests = [{
   fName: 'addition',
   sName: 'template',
 
-  value: 0,
   first: function () {  
     var a = 'one',
       b = a + 'two',
@@ -73,7 +145,6 @@ let tests = [{
   fName: 'array',
   sName: 'destructuring [_,_]',
 
-  value: 0,
   first: function () {
     var u = [0, 1, 2, 3, 4],
       a = u[0],
@@ -92,7 +163,6 @@ let tests = [{
   fName: 'array',
   sName: 'destructuring [_,_,_,_,_]',
 
-  value: 0,
   first: function () {
     var u = [0, 1, 2, 3, 4],
       a = u[0],
@@ -110,7 +180,6 @@ let tests = [{
   fName: 'array',
   sName: 'destructuring [_, ,_]',
 
-  value: 0,
   first: function () {
     var u = [0, 1, 2, 3, 4],
       a = u[0],
@@ -129,7 +198,6 @@ let tests = [{
   fName: 'array',
   sName: 'destructuring [_, ,_,...]',
 
-  value: 0,
   first: function () {
     var u = [0, 1, 2, 3, 4],
       a = u[0],
@@ -148,7 +216,6 @@ let tests = [{
   fName: 'destructuring var [_, ,_,...]',
   sName: 'destructuring let [_, ,_,...]',
 
-  value: 0,
   first: function () {
     var u = [0, 1, 2, 3, 4],
       [a, ,b,...rest] = u;
@@ -166,7 +233,6 @@ let tests = [{
   fName: 'object',
   sName: 'destructuring {_,_}',
 
-  value: 0,
   first: function () {
     var u = {a: 1, b: 2},
       a = u.a,
@@ -175,6 +241,19 @@ let tests = [{
   second: function () {
     var u = {a: 1, b: 2},
       {a, b} = u;
+  }
+}, {
+  name: 'creating objects 1',
+  fName: 'object {a:a}',
+  sName: 'object {a}',
+
+  first: function () {
+    var a = 1, b = 2, c = 3,
+      o = {a: a, b: b, c: c};
+  },
+  second: function () {
+    var a = 1, b = 2, c = 3,
+      o = {a, b, c};
   }
 }];
 
@@ -192,15 +271,20 @@ let tests = [{
     let res1 = runFunctionTest(test.fName, N_REPETITIONS, N_INTERNAL_LOOP_REPS, test.first.bind(test));
     let res2 = runFunctionTest(test.sName, N_REPETITIONS, N_INTERNAL_LOOP_REPS, test.second.bind(test));
 
+    console.log(res1, res2);
+
+    let diff1 = res1.measures[0].duration,
+      diff2 = res2.measures[0].duration;
+
     let styleFast = "color: green;font-weight:bold;",
       styleSlow = "color: red;font-weight:bold;";
 
     let style1 = "",
       style2 = "";
 
-    if (res1.diff === res2.diff) {
+    if (diff1 === diff2) {
 
-    } else if (res1.diff < res2.diff) {
+    } else if (diff1 < diff2) {
       style1 = styleFast;
       style2 = styleSlow;
     } else {
@@ -208,24 +292,33 @@ let tests = [{
       style1 = styleSlow;
     }
 
-    console.log("%c%s", style1, `${test.fName}: ${res1.diff} | av/loop: ${res1.avMsPerLoop} `);
-    console.log("%c%s", style2, `${test.sName}: ${res2.diff} | av/loop: ${res2.avMsPerLoop} `);
+    console.log("%c%s", style1, `${test.fName}: ${diff1} | av/loop: ${res1.avMsPerLoop} `);
+    console.log("%c%s", style2, `${test.sName}: ${diff2} | av/loop: ${res2.avMsPerLoop} `);
 
     console.log('');
   }
 
   function runFunctionTest(testName, nRepetitions, nLoopRepetitions, func) {
-    let sttTime = (new Date()).getTime();
+    performance.mark(`${testName}-start`);
     for (let i = 0; i < nRepetitions; i++) {
       func(nLoopRepetitions);
     }
-    let endTime = (new Date()).getTime();
+    performance.mark(`${testName}-end`);
+
+    performance.measure(testName, `${testName}-start`, `${testName}-end`);
+    let measures = performance.getEntriesByName(testName);
+
+    performance.clearMarks();
+    performance.clearMeasures();
 
     return {
-      diff: endTime - sttTime,
-      avMsPerLoop: (endTime - sttTime) / nRepetitions
+      measures
     };
   }
 
   tests.map(runPerformanceTest);
 })(tests);
+
+
+
+}
